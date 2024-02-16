26b31b9f8bc508c3dfe2f61f87b0c9972d5c2f09
Kiptoo <denysmutay@gmail.com>
2024-02-16 23:55:36 +0200

    model to add student details such as first_name

--- biodata/models.py
+++ biodata/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    reg_no = models.IntegerField()
+    grade = models.CharField(max_length=2)
